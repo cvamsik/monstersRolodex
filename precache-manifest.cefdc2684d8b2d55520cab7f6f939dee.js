c1cb7ccc434f71f8cc159f348452450caf6db7d1
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-02-11 16:53:31 -0800

    Updates

---
+++ precache-manifest.cefdc2684d8b2d55520cab7f6f939dee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2539f6391cd889306a83d39789d1cd",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "fc51d77f49667ff0f09d",
+    "url": "/monstersRolodex/static/css/main.65247ea9.chunk.css"
+  },
+  {
+    "revision": "36ad41339dcfa9f155b0",
+    "url": "/monstersRolodex/static/js/2.11f969c0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monstersRolodex/static/js/2.11f969c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc51d77f49667ff0f09d",
+    "url": "/monstersRolodex/static/js/main.2a2a351b.chunk.js"
+  },
+  {
+    "revision": "c5b42afb9b7a3b1531fb",
+    "url": "/monstersRolodex/static/js/runtime-main.d9171443.js"
+  }
+]);
